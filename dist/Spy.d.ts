fbbe68ed4c79e6ea7f7f91d903cb35b4fc9f35b9
listingslab <listingslab@gmail.com>
2023-07-20 22:01:37 +0200

    Public Growspy NPM package" (#2)

---
+++ dist/Spy.d.ts
@@ -0,0 +1,2 @@
+import * as React from "react";
+export declare function Spy(): React.JSX.Element;
---
+++ dist/Spy.js
@@ -0,0 +1,4 @@
+import * as React from "react";
+export function Spy() {
+    return React.createElement(React.Fragment, null, "Spy");
+}
